abf5c13459886e7900b848feb335c7bdb6bf4320
PingHe87 <heping.m87@gmail.com>
2024-04-24 20:16:03 -0500

    modify models

--- routes/query_instructor_routes.py
+++ routes/query_instructor_routes.py
@@ -10,3 +10,9 @@ def query_form():
     instructor_names = db.session.query(Instructors.name).distinct().all()
     instructor_names = [name[0] for name in instructor_names]  
     return render_template('dataQueryPage/query_instructor.html', instructors_names=instructor_names)
+
+@query_instructor_routes.route('/instructor_query', methods=['GET'])
+def instructor_query():
+    Instructor_name = request.args.get('instructor_name')
+    return redirect(url_for('query_degree_routes.list_sections_by_instructors',
+                            instructor_name=Instructor_name))
